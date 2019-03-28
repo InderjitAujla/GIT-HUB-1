04ce4d2f67379ee1eeda290995ef3b8244ab72bb
InderjitAujla <47224364+InderjitAujla@users.noreply.github.com>
2019-03-28 17:01:21 -0400

    459

--- GIT HUB 1/Program.cs
+++ GIT HUB 1/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace GIT_HUB_1
+namespace c0727119
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Peter Rules the IT Universe!");
+Download();
+Console.ReadLine();
+}
+
+static async void Download()
+{
+await Network.Download();
+Console.WriteLine("Download Complete"); 
+}
+class Network
+{
+static public Task Download()
+{
+return Task.Run( ()=> Thread.Sleep(3000));
+}
+}
 }
